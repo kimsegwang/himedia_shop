346d84098bcbe9c5feca9732e899ee0cf89b85fd
kimsegwang <jjjkk24@gmail.com>
2024-10-14 14:59:07 +0900

    Initial commit

---
+++ src/main/java/com/example/tobi/himedia_shop/HimediaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobi.himedia_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimediaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimediaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tobi/himedia_shop/HimediaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobi.himedia_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimediaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
